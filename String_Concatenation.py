3e24094ad9d83e468f7f8b065bc20c87aee98cd0
Elliot Stenning <elliots@elliotcomputerguy.com>
2021-04-16 11:16:34 +0100

    String Concatenation

--- String_Concatenation.py
+++ String_Concatenation.py
@@ -1,4 +1,5 @@
-# String concatenation with the + operator. You can add together two string values
+#!/usr/bin/python
+# String concatenation can be achieved with the + operator. You can add together two string values
 # by using the + operator. 
 var = 'Hello' + 'World'
 print(var)
