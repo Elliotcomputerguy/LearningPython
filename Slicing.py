f1f0ceac59b33b7076c156d000c214670e7e436f
Elliot Stenning <elliots@elliotcomputerguy.com>
2021-04-16 22:42:42 +0100

    Slicing And Blank Slice Indexes

--- Slicing.py
+++ Slicing.py
@@ -18,3 +18,24 @@ print(var)
 
 # Notice how 0 and -0 are the same. Remember that -1 is the last character in negative indexes 
 
+# Unlike indexes, slicing will never give you an error if you give it too large of an index for the string.
+
+var = 'Hello world'[0:999]
+print(var)
+var = 'Hello world'[2:999]
+print(var)
+var = 'Hello world'[1000:2000]
+print(var)
+
+# #Blank Slice Indexes
+# If you leave out the first index of a slice, python will automatically think you want to specify index 0 for the first index. 
+# The expression 'Hello'[0:3] and 'Hello'[:3] evaluate to the same string.
+
+var = 'Hello world'[:3]
+print(var)
+
+# If you leave out the second index, python will automatically think you want to specify the rest of the string. 
+# 'Hello'[2:] evaluates to the value 'llo'
+
+var ='Hello world'[2:]
+print(var)
