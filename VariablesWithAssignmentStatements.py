9883425a3f11592207d8339dcb06beca6b4f2091
Elliot Stenning <elliots@elliotcomputerguy.com>
2021-04-16 11:19:00 +0100

    AssignmentStatements

---
+++ VariablesWithAssignmentStatements.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# The below assigment statement assigns the datatype string value Hello World to the variable called var. 
+# The print() function call performs a task by printing the value to the screen.  
+var = 'Hello World'
+print(var)
