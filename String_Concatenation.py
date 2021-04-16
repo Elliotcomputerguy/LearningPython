e5591ee5a211df240456a2b86d3c7bb71575c8a2
Elliot Stenning <elliots@elliotcomputerguy.com>
2021-04-16 11:39:05 +0100

    Remove file

--- String_Concatenation.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-# String concatenation can be achieved with the + operator. You can add together two string values
-# by using the + operator. 
-var = 'Hello' + 'World'
-print(var)
