8f47917703ae318be135b4b15cfa7672676b7ff6
Elliot Stenning <elliots@elliotcomputerguy.com>
2021-04-16 12:16:24 +0100

    Print Function

---
+++ PrintFunction.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# There is another python instruction called a print() function call. It performs a task to print values to the screen.
+# There are many different functions that come with Python. To call a function means to execute the code that is inside
+# the function parenthesis().
+# A value that is passed into the function call are called arguments. Arguments are the same as values but are only called 
+# arguments when they are passed to function calls. You can also pass an expression to the print() function call instead of a
+# single value. This is becasue the value that is actually passed to the print() function is the evaluated value of that expression.
+
+var = 'Elliot'
+print('Hello, ' + var)
