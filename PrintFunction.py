7866948161d236912f2fa6692b18d19e64d1bc19
Elliot Stenning <elliots@elliotcomputerguy.com>
2021-04-16 23:18:37 +0100

    Print Function

--- PrintFunction.py
+++ PrintFunction.py
@@ -4,7 +4,7 @@
 # the function parenthesis().
 # A value that is passed into the function call are called arguments. Arguments are the same as values but are only called 
 # arguments when they are passed to function calls. You can also pass an expression to the print() function call instead of a
-# single value. This is becasue the value that is actually passed to the print() function is the evaluated value of that expression.
+# single value. This is because the value that is actually passed to the print() function is the evaluated value of that expression.
 
 var = 'Elliot'
 print('Hello, ' + var)
