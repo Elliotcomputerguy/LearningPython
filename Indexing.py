d0160f5f1da3fe183d82240b2da807c3efac584c
Elliot Stenning <elliots@elliotcomputerguy.com>
2021-04-16 13:37:38 +0100

    Indexing

---
+++ Indexing.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# Indexing is the adding of a square bracket to the end of a string value or a variable containing a string with a number 
+# between them. This number is called the index, and tells python which postion in the string has the character you want. 
+# The index of the first character in a string is 0. The index 1 is the second character, etc. 
+
+var = 'Elliot'
+print(var[0],var[1],var[2],var[3],var[4],var[5])
+var = 'Elliot'[1]
+
+
+
