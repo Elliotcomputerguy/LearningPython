b148a45b99586b565f38d356bfc8233833b9e014
Elliot Stenning <elliots@elliotcomputerguy.com>
2021-04-18 23:06:29 +0100

    Range Function

---
+++ RangeFunction.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+# The range() function
+# The range function takes one integer argument and returns a value of the range data type. These range values can be used in for loops to
+# loop a specific number of times. 
+
+for i in range(5):
+    print('Elliot')
+
+for i in range(6):
+    print(i)
+
+# You can also pass two integer arguments to the range() function. The first argument is where it should start from with the second
+# argun=ment being where it should finish. 
+print('---------------------------')
+for i in range(3, 8):
+    print(i)
