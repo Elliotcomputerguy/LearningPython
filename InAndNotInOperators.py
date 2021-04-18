9fc4eb2110b6507723bbfa793672002cd939cf2d
Elliot Stenning <elliots@elliotcomputerguy.com>
2021-04-18 17:16:00 +0100

    In And Not In Operators

---
+++ InAndNotInOperators.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# The in and not in Operators
+# An expression of two strings connected by the in operator will evaluate to True if the first string is inside the second string
+# Otherwise the expression evaluates to False. Notice that the in and the not in operators are case sensitive. 
+
+
+var = 'Hello world'
+if 'Hello' in var:
+    print('True')
+else:
+    print('False')
