1c02707d8977bab9f8db80ba8ca3bb7c1961c2b8
Elliot Stenning <elliots@elliotcomputerguy.com>
2021-04-19 20:40:52 +0100

    Def Function

---
+++ CreateYourOwnFunction.py
@@ -0,0 +1,10 @@
+#!/bin/usr/python
+
+# The def statement means we are creating or defining a new function. We can call the function later in your program
+
+def myFunction():
+    print('This is my function')
+    total = 42 + 1
+    print('42 plus 1 is %s' % (total))
+print('start!')
+myFunction()
