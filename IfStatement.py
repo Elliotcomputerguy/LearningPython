fb676c56438c372746fd401ba8baf16bcb197828
Elliot Stenning <elliots@elliotcomputerguy.com>
2021-04-23 23:41:37 +0100

    If Statement

--- IfStatement.py
+++ IfStatement.py
@@ -12,7 +12,10 @@ print('Done')
 
 # Interpreting any value as True or False 
 # Below your see no comparison operator but the value itself is being treated as the condition. I caled it condition. 
-# When it comes to evaluating a number as a condition. 0 is Null and everything else is True.
+# When it comes to evaluating a number as a condition. 0 is False and everything else is True.
+
+# In fact, there are not many values that evaluate to False, except empty values, such as (), [], {}, "", 
+# the number 0, and the value None. And of course the value False evaluates to False.
 
 condition = input('\nEnter a value to mark condition as True or hit enter to skip to mark as False:')
 if condition:
