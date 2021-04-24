2616d6acdcb4c90fc3f3e762af7d76a5c25f0cf7
Elliot Stenning <elliots@elliotcomputerguy.com>
2021-04-24 12:38:09 +0100

    Remove file

--- VariablesWithAssignmentStatements.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-# The below assigment statement assigns the datatype string value Hello World to the variable called var. 
-# The print() function call performs a task by printing the value to the screen.  
-var = 'Hello World'
-print(var)
