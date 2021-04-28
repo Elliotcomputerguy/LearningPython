6d3a5dfb1b06e33fb6ee956ab6014c7ff8438c20
Elliot Stenning <elliot@elliotcomputerguy.com>
2021-04-28 17:52:38 +0100

    slicing

--- Slicing.py
+++ Slicing.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 
-# #Slicing
-# If you want to get more than once character from a string, you can use slicing of indexing.
-# A slice also uses the [ and ] square brackets but has two integer indexes instead of one.
+# If you want to get more than one character from a string, list or a tuple, you can use slicing of indexing.
+# A slice also like indexes or lists uses the [  ] square brackets but has two integer indexes instead of one.
 # The two indexes are seperated by a colon : 
+
+#  0   1   2   3   4   5
+#  +---+---+---+---+---+
+#  | i | n | d | e | x |
+#  +---+---+---+---+---+
+#  -5 -4  -3  -2  -1
 
 # ===============================================================
 # Example:
 
-var = 'Elliot'[0:3]
+var = 'ElliotComputerGuy'[0:3]
 print(var)
 var = 'Hello World'[0:5]
 print(var)
@@ -20,11 +25,8 @@ var = 'Hello world'[-0]
 print(var)
 
 # ===============================================================
-
 # Notice how 0 and -0 are the same. Remember that -1 is the last character in negative indexes 
-
 # Unlike indexes, slicing will never give you an error if you give it too large of an index for the string.
-
 # ===============================================================
 # Example:
 
@@ -36,15 +38,9 @@ var = 'Hello world'[1000:2000]
 print(var)
 
 # ===============================================================
-
-# #Blank Slice Indexes
-
-# If you leave out the first index of a slice, python will automatically think you want to specify index 0 for the first index. 
+# Blank Slice Indexes is when you leave out the first index of a slice. 
+# Python will automatically think you want to specify index 0 for the first index. 
 # The expression 'Hello'[0:3] and 'Hello'[:3] evaluate to the same string. 'Hel'
-
-# to help make it more visibly clearer. Put a 0 in if the slice is after the colon. Then take it out. 
-# This helped me when learning as into what direction.
-
 # ===============================================================
 # Example:
 
@@ -52,10 +48,8 @@ var = 'Hello world'[:3] # <-- Get only characters before 3 = 'Hel' Everything up
 print(var)
 
 # ===============================================================
-
 # If you leave out the second index, python will automatically think you want to specify the rest of the string. 
 # 'Hello'[2:] evaluates to the value 'llo world'
-
 # ===============================================================
 # Example:
 
@@ -63,43 +57,25 @@ var ='Hello world'[2:] # <-- Get characters only After 2 = 'llo world'
 print(var)
 
 # ===============================================================
-
-# #Reassigning characters in strings
-
-# While you can reassign items in a list, you cannot assign a character in a string value. Strings are immutable. 
-# You have to use slicing instead
-
-# Below i am changing the character h with G. So i want to start with :0 (Everthing up to :0). I then finish with 1: (Everything after).
-# This will now replace h with g. Practice this a couple of times as it took me a little while to get this to sink in. 
-# Key. Rember the directions.
-
+# You cannot assign a character in a string value as strings are immutable.
+# using slicing you can change the character of a string without assiging the variable another value. 
 # ===============================================================
 # Example:
 #  
-# 0    1    2    3    4   5
-# [ h ][ e ][ l ][ l ][ o ]
+#  0    1    2    3    4   5
+#  [ h ][ e ][ l ][ l ][ o ]
 # -5  -4    -3   -2   -1   
 
 var = 'Hello'
 var = var[:0] + 'G' + var[1:]
 print(var)
 
-# ===============================================================
-
-# ===============================================================
-# Example:
-
-# What character do you think i am changing here ? Remember the index it evalautes too is the charachter i am slicing. 
-# It is that simple. Do not over think it. 
-
 var = 'string'
 var = var[:4] + 'x' + var[5:]
 print(var)
 
 # ===============================================================
-
-# Slicing is a simple way to get a substring from a larger string.
-
+# Slicing pizza game
 # ===============================================================
 # Example:
 
@@ -126,14 +102,32 @@ while start != '':
 print('finish')    
 
 # ===============================================================
-
+# remove a character from the string assigned to word using slicing
 # ===============================================================
 # Example:
 
-# remove a character from the string assigned to word using slicing
-word = 'mcdonalds'
+word = 'sneakers'
 print('Original variable word:', word)
 word = word[:4] + '' + word[5:]
 print('New word:', word)
 word = word[:4] + word[(4 + 1):] 
-print(word)
+
+# ===============================================================
+# using slices to slice a list 
+# ===============================================================
+# Example:
+
+listOfNums = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+newListOfNums = listOfNums[3:8] #<---------------------- Slicing will display up to the last slice but not the last slice. 
+print(newListOfNums) # = [40, 50, 60, 70, 80]
+
+# ===============================================================
+# Slice assigment
+# ===============================================================
+# Example:
+
+listOfNums = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+listOfNums[:4] = [11,22,33,44]
+print(listOfNums)
+
+# ===============================================================
