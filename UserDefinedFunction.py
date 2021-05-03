1fc9edac7dcfcd4ac74856db31b74cbe84373c1f
Elliot Stenning <elliot@elliotcomputerguy.com>
2021-05-03 20:39:55 +0100

    User Defined Function

--- UserDefinedFunction.py
+++ UserDefinedFunction.py
@@ -8,8 +8,10 @@
 # This will be the first line of the function. The function body contains the block of code that will execute everytime 
 # the function is called. When you want to find out what a function does you can call the help() function with the function inside
 # the parentheses. In order to let other people know what your function does you would create a docstring which is a triple quoted string
-# at the top of your function. A function does not have to have any paramaters. Side effects are known as unexpected return values that
-# are not intended. 
+# at the top of your function. A function does not have to have any paramaters. Functions can be Idempotent or non Idempotent. 
+# Basically in a nutshell a function that returns the same result everytime it is called, regardless of how many times it is called. 
+# It is Idempotent. If different results return then this is non Idempotent and is also called a side effect. Test your functions with all
+# input calls to ensure that your function provides the desired outcome every time regardless of what calls are made to it.
 # ===============================================================
 # Example:
 
@@ -85,12 +87,12 @@ print(userProfileArgrument)
 
 # is using global scope and using positional parameters
 
-userProfileList = []  #<------------- Global scope
+userProfileList = []  #<---------- Global scope
 def userProfile0(forename, surname, age):
     
-    userProfileList.append(forename.title().strip()) #<------------- Global scope
-    userProfileList.append(surname.title().strip())  #<------------- Global scope 
-    userProfileList.append(age)                      #<------------- Global scope
+    userProfileList.append(forename.title().strip()) #<---------- Global scope
+    userProfileList.append(surname.title().strip())  #<---------- Global scope
+    userProfileList.append(age)                      #<---------- Global scope
 
     if forename and surname and age in userProfileList:
         userInfo = 'Hello, %s, your information has been stored...' % (forename)
@@ -133,60 +135,6 @@ def sneakerStock(*args):
 sneakerStock('model', 'stock', '7')
 
 # ===============================================================
-# Calling a function from another function is what functions are designed for. 
-# 
-
-def passwordCreator():
-    ''' Generate a number of choice of randomized passwords with the choice of password length '''
-    print('''
-        
-██████   █████  ███████ ███████ ██     ██  ██████  ██████  ██████       ██████  ███████ ███    ██ ███████ ██████   █████ ████████  ██████  ██████  
-██   ██ ██   ██ ██      ██      ██     ██ ██    ██ ██   ██ ██   ██     ██       ██      ████   ██ ██      ██   ██ ██   ██   ██    ██    ██ ██   ██ 
-██████  ███████ ███████ ███████ ██  █  ██ ██    ██ ██████  ██   ██     ██   ███ █████   ██ ██  ██ █████   ██████  ███████   ██    ██    ██ ██████  
-██      ██   ██      ██      ██ ██ ███ ██ ██    ██ ██   ██ ██   ██     ██    ██ ██      ██  ██ ██ ██      ██   ██ ██   ██   ██    ██    ██ ██   ██ 
-██      ██   ██ ███████ ███████  ███ ███   ██████  ██   ██ ██████       ██████  ███████ ██   ████ ███████ ██   ██ ██   ██   ██     ██████  ██   ██ 
-
-        
-        ''')
-
-    userPasswordLength = input('[+] Please enter the length size 0 - 50:>')
-    userGeneratedPasswords = input(' [+] How many passwords would you like to create:>')
-    userPassList = [jumbleWord(userPasswordLength) for _ in range(userGeneratedPasswords)]
-    generatedPasswordList = f' [+] Your list has been created using {userPasswordLength} as your pass length\n {userPassList}'
-    return generatedPasswordList
-
-
-def jumbleWord(lengthNumber):
-    ''' jumbleWord creates a random string from the character set abcdefghijklmnopqrstuvwxyz1234567890!"£$%&*()@?~#:;
-        The function expects an integer argument to return a string size from the character set. 
-        Example: jumbleWord(10) will return a randomized string size of 10 characters from the above character set. 
-    '''
-    import random
-    passLength = lengthNumber
-    jumbledWord = ''
-    CHARACTERS = 'abcdefghijklmnopqrstuvwxyz1234567890!"£$%&*()@?~#:;'
-    for characters in CHARACTERS:
-        randomizer = random.choice(CHARACTERS)
-        if len(jumbledWord) < passLength:
-            jumbledWord += randomizer
-    return jumbledWord
-
-
-
-
-
-
-def Yes_Or_No(param):
-    answerTuple = ('yes', 'no')
-    for i in range(len(answerTuple)):
-        if param.lower().strip() == answerTuple[i]:
-            answer = answerTuple[i]
-            return answer
-
-userQuestion = input('Enter Yes or no:')
-print(Yes_Or_No(userQuestion))
-
-# ===============================================================
 # When a function is called, a local scope is created. Variables created during a function call exist in the local scope.
 # Parameters always exist in a local scope. When a function returns, the local scope is destroyed and all variables are forgotten. 
 # A variable in the local scope is still a sperate variable from a global scope variable even if the two variables have the same name.
@@ -201,32 +149,83 @@ print(Yes_Or_No(userQuestion))
 # 2) If a variable in a function is never used in an 'assigment statement', it is a global variable.
 # 3) If a variable in a function is not used in a 'global statement'; but is used in an assigment statement, it is a local variable
 # 4) If a variable in a function is used in a 'global statement' it is a global variable when used in that function.
-
-
-
-# Convert Temprature functions.
 # ===============================================================
+# Example:
 
-def convert_cel_to_far(c):
-    f = c * 9/5 + 32
-    return float(f)
+def Yes_Or_No(param):
+    answerTuple = ('yes', 'no') #<----------- Local scope
+    for i in range(len(answerTuple)):
+        if param.lower().strip() == answerTuple[i]:
+            answer = answerTuple[i]
+            global var #<-------------------------------Global scope
+            print(var)
+            return answer
 
-userCelciusConvert = int(input('Enter a Celsius temprature to convert to Farneheit:'))
-print(convert_cel_to_far(userCelciusConvert))
+var = 'i am a global variable'
+userQuestion = input('Enter Yes or no:') #<------ Global scope
+print(Yes_Or_No(userQuestion))
 
-def convert_far_to_cel(f):
-    c = (f - 32) * 5/9
-    return float(c)
+# ===============================================================
+# Functions help segement code blocks and make code clearer to read. You can further this readability by storing your functions as 
+# modules in a seperate py file and then import them as modules into your main program. There are ways to achieve this. 
+# You save your functions to seperate .py files in the same directory to the py file you call the modules from. You would type the 
+# import statement as you would if you were importing math or range. Python would read the line and open the file and copy the functions
+# across making it available. When calling the function from the imported module you would enter the name of the module that 
+#  you imported followed by the name of the function, seperated by a dot. moduleName.functionName() 
+# The second method is to import a specific function from the module. At the top of your program that you wish to import
+# the modules into. You would type 'from moduleName import functionName' You can import as you want by sperating by a comma. 
+# Using this syntax you do not need to use the dot notation when you call a function. Because we have explicitly imported the function.
+# With the second way we can specify an alias if the function name provides any confliction. 'from module import function as alias'
+# If you wist to import all functions in a module you can specify the * wildcard. 'from moduleName import *'
+# ===============================================================
+# Example:
 
-userFarneheitConvert = int(input('Enter a Farenheit temprature to convert to Celsius:'))
-print(convert_far_to_cel(userFarneheitConvert))
+# [1] save below function as passwordCreator.py in a directory of choice. 
+# [*] As passwordCreator() calls jumbleWord() you import jumbleWord() inside the passwordGenerator() function.
+# [2] save the second function marked with [2] as jumbleWord.py in the same directory.
+# [3] create a new file .py and import passwordCreator and then call it. 
 
+def passwordCreator():
+    from jumbleWord import jumbleWord
+    ''' Generate a number of choice of randomized passwords with the choice of password length '''
+    print('''
+        
+██████   █████  ███████ ███████ ██     ██  ██████  ██████  ██████       ██████  ███████ ███    ██ ███████ ██████   █████ ████████  ██████  ██████  
+██   ██ ██   ██ ██      ██      ██     ██ ██    ██ ██   ██ ██   ██     ██       ██      ████   ██ ██      ██   ██ ██   ██   ██    ██    ██ ██   ██ 
+██████  ███████ ███████ ███████ ██  █  ██ ██    ██ ██████  ██   ██     ██   ███ █████   ██ ██  ██ █████   ██████  ███████   ██    ██    ██ ██████  
+██      ██   ██      ██      ██ ██ ███ ██ ██    ██ ██   ██ ██   ██     ██    ██ ██      ██  ██ ██ ██      ██   ██ ██   ██   ██    ██    ██ ██   ██ 
+██      ██   ██ ███████ ███████  ███ ███   ██████  ██   ██ ██████       ██████  ███████ ██   ████ ███████ ██   ██ ██   ██   ██     ██████  ██   ██ 
 
+        
+        ''')
 
+    userPasswordLength = int(input('[+] Please enter the length size 0 - 50:>')) #<---------- Local scope
+    userGeneratedPasswords = int(input(' [+] How many passwords would you like to create:>')) #<---------- Local scope
+    userPassList = [jumbleWord(userPasswordLength) for _ in range(userGeneratedPasswords)] #<---------- Local scope
+    generatedPasswordList = f' [+] Your list has been created using {userPasswordLength} as your pass length\n {userPassList}' #<---------- Local scope
+    return generatedPasswordList
 
+# [2] save below function as jumbleWord.py
 
+def jumbleWord(lengthNumber):
+    ''' jumbleWord creates a random string from the character set abcdefghijklmnopqrstuvwxyz1234567890!"£$%&*()@?~#:;
+        The function expects an integer argument to return a string size from the character set. 
+        Example: jumbleWord(10) will return a randomized string size of 10 characters from the above character set. 
+    '''
+    import random
+    passLength = lengthNumber #<---------- Local scope
+    jumbledWord = '' #<---------- Local scope
+    CHARACTERS = 'abcdefghijklmnopqrstuvwxyz1234567890!"£$%&*()@?~#:;'
+    for characters in CHARACTERS:
+        randomizer = random.choice(CHARACTERS)
+        if len(jumbledWord) < passLength:
+            jumbledWord += randomizer
+    return jumbledWord
 
+# [3] Create a new file .py and add the below.
 
+from passwordCreator import passwordCreator
 
+print(passwordCreator())
 
-     
+# ===============================================================
