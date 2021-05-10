88711738322bf2a929e5f9f06c144ca75c00c452
Elliot Stenning <elliot@elliotcomputerguy.com>
2021-05-10 14:44:02 +0100

    Booelan Comparators

--- BooleanComparators.py
+++ BooleanComparators.py
@@ -1,9 +1,8 @@
 
-
-
-# Boolean comparators are used to compare two values. Boolean became known as the term Boolean coined by the mathematician George Boole.
-# conditonals are sometimes expressed as Boolean expressions and conditional logic refered to as Boolian logic. Boolean can only evaluate
-# a conditonal to two Boolean values. Either True or False. True and False both start with a capital letters. 
+# Boolean comparators are used to compare two values. True and False. Boolean became known as the term Boolean coined by 
+# the mathematician George Boole. Conditonals are sometimes expressed as Boolean expressions and conditional logic refered 
+# to as Boolian logic. Boolean can only evaluate a conditonal to two Boolean values. Either True or False. 
+# True and False both start with a capital letters. 
 # ===============================================================
 
 ###################################################
